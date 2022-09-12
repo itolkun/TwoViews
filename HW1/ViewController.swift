7a849a50db6417b7016244b992925d4732db66c1
Aitolkun <totita@list.ru>
2022-09-13 02:08:44 +0600

    Initial Commit

---
+++ HW1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW1
+//
+//  Created by Айтолкун Анарбекова on 13/9/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
